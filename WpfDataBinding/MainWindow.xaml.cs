905cb391296dea507c2b05e1244abd0f066b1782
Pavel Svec <pavel.svec@itcrawl.cz>
2023-01-22 22:42:52 +0100

    Styling, staticresources, ListView

--- WpfDataBinding/MainWindow.xaml.cs
+++ WpfDataBinding/MainWindow.xaml.cs
@@ -3,14 +3,18 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Microsoft.Xaml.Behaviors;
 using System.Threading;
 using System.Windows.Threading;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace WpfDataBinding
 {
     class Person : INotifyPropertyChanged
     {
+        public static List<Person> AllPersons { get; set; } = new List<Person>();
+
         private string _Jmeno, _Prijmeni;
         private DateTime _Narozeni;
 
@@ -64,6 +68,12 @@ namespace WpfDataBinding
             if (PropertyChanged != null) // jestli někdo poslouchá ...
                 PropertyChanged(this, new PropertyChangedEventArgs(property));
         }
+
+        public static Person PersonCopy(Person p)
+        {
+            Person q = new Person() { Jmeno = p.Jmeno, Prijmeni= p.Prijmeni, Narozeni=p.Narozeni };
+            return q;
+        }
     }
 
     /// <summary>
@@ -92,6 +102,9 @@ namespace WpfDataBinding
 
             MessageBox.Show(p.ToString() + "\n" + 
                 expr.ResolvedSourcePropertyName + ": vynucená akutalizace");
+            Person.AllPersons.Add(Person.PersonCopy(p));
+            lv.ItemsSource = null;
+            lv.ItemsSource = Person.AllPersons;
         }
 
         // Změna dat, vzhledem k bindingu, stačí i k aktualizaci grafické podoby formuláře
@@ -99,6 +112,13 @@ namespace WpfDataBinding
         {
             p.Jmeno = p.Prijmeni = string.Empty;
             p.Narozeni = DateTime.Now;
+        }
+
+        private void btDefault_Click(object sender, RoutedEventArgs e)
+        {
+            p.Jmeno = "Jan";
+            p.Prijmeni = "Novák";
+            p.Narozeni = new DateTime(1990, 1, 1);
         }
     }
 
